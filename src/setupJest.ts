b1c637f7996acda658c2e0690f176e8af73ed797
Brandon Roberts <brandon.roberts@synapse-wireless.com>
2018-04-16 23:18:04 -0500

    Add Jest and jasmine-marbles

---
+++ src/setupJest.ts
@@ -0,0 +1,11 @@
+import 'jest-preset-angular';
+global['CSS'] = null;
+
+Object.defineProperty(document.body.style, 'transform', {
+  value: () => {
+    return {
+      enumerable: true,
+      configurable: true,
+    };
+  },
+});
